dd362b355d9bf148edfb697979945a7b2b31be80
SajjalMalik <sajjalmalik.sm@gmail.com>
2024-07-16 20:09:00 +0500

    Removed the c execution file from Practice Set of Chatper 5 and modified some files also

--- Practice Set Chap 5/Problem_2.c
+++ Practice Set Chap 5/Problem_2.c
@@ -1,3 +1,5 @@
+// Write a function to convert Celsius temperature into Fahrenheit.
+
 #include<stdio.h>
 
 // Function prototype
--- Practice Set Chap 5/Problem_3.c
+++ Practice Set Chap 5/Problem_3.c
@@ -1,3 +1,6 @@
+// Write a function to calculate force of attraction on a body of mass ‘m’ exerted by
+// earth. Consider g = 9.8m/s2
+
 #include<stdio.h>
 
 // Function prototype
